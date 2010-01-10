50018511c31e22a0256631f65afb0b0d8afc2ae5
James Blomo <jim@crow.gateway.2wire.net>
2010-01-10 15:16:14 -0800

    better not_much_data measure

--- graph/views.py
+++ graph/views.py
@@ -133,7 +133,7 @@ def user_metric_detail(request, screen_name, metric):
                 'table_json': jscode,
                 'metric': metric,
                 'screen_name': screen_name,
-                'data_length': len(data),
+                'not_much_data': data[0]['created_at'] > datetime.now()-timedelta(3),
                 'most_recent': data.pop() if data else None,
                 })
     
